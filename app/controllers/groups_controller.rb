d74012519afdafefd4055cd5954b13d6012bb906
zhangshuo <3014zhangshuo@gmail.com>
2016-10-02 08:57:47 +0800

    add

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,6 +11,7 @@ class GroupsController < ApplicationController
 
   def show
     @group = Group.find(params[:id])
+    @posts = @group.posts
   end
 
   def edit
@@ -42,7 +43,7 @@ class GroupsController < ApplicationController
 
   private
   def find_group_and_check_permission
-    @group = Group.find(parmas[:id])
+    @group = Group.find(params[:id])
     if current_user != @group.user
       redirect_to root_path alert: "You have no permission."
     end
